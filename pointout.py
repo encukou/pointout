82e327a32324dd2d02632690fff1ce26d19f2419
Petr Viktorin <encukou@gmail.com>
2020-04-22 02:56:17 +0200

    Attempt drawing with less than full opacity

--- pointout.py
+++ pointout.py
@@ -1,6 +1,7 @@
 import sys
 from PySide2.QtWidgets import QApplication, QWidget
 from PySide2.QtGui import QPainter, QColor, QPixmap, QPen, QTabletEvent
+from PySide2.QtGui import QPainterPath
 from PySide2.QtCore import Qt, QEvent, QRect
 
 MAX_RADIUS = 100
@@ -23,22 +24,30 @@ class Overlay(QWidget):
 
         self.pixmap = QPixmap(self.width(), self.height())
         self.pixmap.fill(QColor(0, 0, 0, 1))
+        self.stroke_pixmap = QPixmap(self.width(), self.height())
+        self.stroke_pixmap.fill(QColor(0, 0, 0, 1))
 
         self.setCursor(Qt.CrossCursor)
 
-    def resizeEvent(self, e):
+    def _resize_pixmap(self, old_pixmap):
         new_pixmap = QPixmap(self.width() , self.height())
         new_pixmap.fill(QColor(0, 0, 0, 1))
         painter = QPainter(new_pixmap)
-        painter.drawPixmap(0, 0, self.pixmap)
+        painter.drawPixmap(0, 0, old_pixmap)
         painter.end()
-        self.pixmap = new_pixmap
+        return new_pixmap
+
+    def resizeEvent(self, e):
+        self.pixmap = self._resize_pixmap(self.pixmap)
+        self.stroke_pixmap = self._resize_pixmap(self.stroke_pixmap)
 
     def paintEvent(self, e):
         painter = QPainter(self);
-        painter.setBrush(QColor(0, 0, 0, 100))
+        painter.setBrush(QColor(0, 0, 0, 0))
         #painter.drawRect(e.rect());
-        painter.drawPixmap(e.rect().topLeft(), self.pixmap, e.rect());
+        painter.drawPixmap(e.rect().topLeft(), self.pixmap, e.rect())
+        painter.setOpacity(0.5)
+        painter.drawPixmap(e.rect().topLeft(), self.stroke_pixmap, e.rect())
         painter.end()
 
     def tabletEvent(self, e):
@@ -47,17 +56,26 @@ class Overlay(QWidget):
         if e.type() == QEvent.TabletPress:
             self.last_point = e.posF(), e.pressure()
         elif e.type() == QEvent.TabletMove:
-            painter = QPainter(self.pixmap)
             if e.pointerType() == QTabletEvent.Eraser:
+                painter = QPainter(self.pixmap)
                 painter.setPen(QPen(QColor(255, 255, 255, 0), e.pressure()**2*MAX_RADIUS))
                 painter.setCompositionMode(QPainter.CompositionMode_Clear)
+                self.paint(painter, e)
+                painter.end()
             else:
-                painter.setPen(QPen(QColor(0, 0, 0, 200), e.pressure()*MAX_RADIUS/10))
-            painter.setRenderHint(QPainter.Antialiasing)
-            self.paint(painter, e)
+                painter = QPainter(self.stroke_pixmap)
+                painter.setPen(QPen(QColor(0, 0, 0, 255), e.pressure()*MAX_RADIUS/10))
+                painter.setRenderHint(QPainter.Antialiasing)
+                self.paint(painter, e)
+                painter.end()
             self.last_point = e.posF(), e.pressure()
         elif e.type() == QEvent.TabletRelease:
-            pass
+            painter = QPainter(self.pixmap)
+            painter.setOpacity(0.5)
+            painter.drawPixmap(0, 0, self.stroke_pixmap)
+            painter.end()
+            self.stroke_pixmap.fill(QColor(0, 0, 0, 1))
+            self.update()
         e.accept()
 
     def paint(self, painter, e):
